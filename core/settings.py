1efaa1bd46fcc486dd6d8fcb9afc8b803d509082
Mohammadi <amohammadia0086@gmail.com>
2026-02-08 02:05:07 +0330

    Convert all template asset links to Django static
    - Updated all CSS, JS, image, and video references in templates to use {% static %} tag.
    - Removed duplicate non-static links.
    - Changed STATICFILES_DIRS to use BASE_DIR / "static" instead of core/static.
    - Ensures proper serving of static files in Django templates.

--- core/settings.py
+++ core/settings.py
@@ -68,8 +68,11 @@ TEMPLATES = [
         },
     },
 ]
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "core/static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"  
