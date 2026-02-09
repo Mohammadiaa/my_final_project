867dfb6d37aeea94ff02513335c8f1115235d244
Mohammadi <amohammadia0086@gmail.com>
2026-02-10 00:29:20 +0330

    Add admin configuration for Blog and Website models
    - Registered Category, Tag, and Post models in admin with custom display:
      list_display, list_filter, search_fields, date_hierarchy, filter_horizontal, ordering.
    - Registered Contact and User models in admin with custom display:
      list_display, list_filter, search_fields, ordering.
    - This makes admin interface more user-friendly and manageable for both apps.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Post, Category, Tag
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+     list_display = ('id', 'name')
+     search_fields = ('name',) 
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'status', 'counted_view', 'published_date', 'created_date')
+    list_filter = ('status', 'categories', 'tags', 'author')
+    search_fields = ('title', 'content', 'author__username')
+    date_hierarchy = 'published_date'
+    filter_horizontal = ('categories', 'tags')
+    ordering = ('-created_date',)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Contact, User
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'subject', 'created_date', 'updated_date')
+    search_fields = ('name', 'email', 'subject', 'message')
+    list_filter = ('created_date', 'updated_date')
+    ordering = ('-created_date',)
+
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'active', 'superuser', 'staff', 'created_date')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('active', 'superuser', 'staff', 'created_date')
+    ordering = ('-created_date',)
