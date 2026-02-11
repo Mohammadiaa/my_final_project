34a2989a032a24c374198c5ab98e9cf5cd3b89f4
Mohammadi <amohammadia0086@gmail.com>
2026-02-11 20:36:03 +0330

    Implement pagination on blog home and update active sidebar links
    - Added Django Paginator to home view to display 4 posts per page
    - Updated template to show Prev/Next buttons conditionally
    - Made page numbers clickable to navigate to respective pages
    - Highlight active sidebar menu item based on current URL
    - General cleanup of old template links

--- website/views.py
+++ website/views.py
@@ -3,13 +3,27 @@ from blog.models import Post
 from website.models import Contact
 from django.utils import timezone
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home_view(request):
-    posts = Post.objects.filter(status = 1, published_date__lte=timezone.now())
+    posts = Post.objects.filter(status = 1, published_date__lte=timezone.now()).order_by("-published_date")
+
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except  PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'website/index.html', {'posts':posts})
+
+
 
 def about_view(request):
     return render(request, 'website/about.html')
+
+
 
 def contact_view(request):
     if request.method == 'POST':
