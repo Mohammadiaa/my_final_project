dfb4937a7510659a84958b1ae9a313d4f75bb578
Mohammadi <amohammadia0086@gmail.com>
2026-02-11 15:31:55 +0330

    Refactor index page to fetch and render posts dynamically
    - Implemented dynamic post fetching using Post model
    - Updated home view to pass published posts to index template
    - Refactored index template to render posts with loop
    - Replaced static content with dynamic post data (title, image, content, categories, date, author)

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 def home_view(request):
-    return render(request, 'website/index.html')
+    posts = Post.objects.filter(status = 1, published_date__lte=timezone.now())
+    return render(request, 'website/index.html', {'posts':posts})
 
 def about_view(request):
     return render(request, 'website/about.html')
